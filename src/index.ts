bfa0d1cdf2e2c9597eea229e7e236d6497f4a011
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-09-09 15:06:42 -0700

    Show custom API provided to untrusted code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
+import { WorkerEntrypoint } from 'cloudflare:workers';
+
+export class Greeter extends WorkerEntrypoint {
+	async greet(name: string) {
+	  return `${this.ctx.props.greeting}, ${name}!`;
+	}
+  }
 
 const untrustedCode = `
 						export default {
 							async fetch(request, env, ctx) {
-								return new Response('Hello from a dynamically loaded worker!')
+								const greeting = await env.CUSTOM_BINDING_API.greet(env.USERNAME);
+								console.log(greeting);
+								return new Response(greeting + " (sent from a dynamically loaded worker)");
 							}
 						};
 					`
@@ -15,6 +24,12 @@ export default {
 				mainModule: "index.js",
 				modules: {
 					"index.js": untrustedCode
+				},
+				env: {
+					// Provide the untrusted code with a custom API
+					CUSTOM_BINDING_API: ctx.exports.Greeter({ props : { greeting: 'Hello' }}),
+					// Provide the untrusted code with any environment variables
+					USERNAME: 'brendan'
 				}
 			};
 		});
